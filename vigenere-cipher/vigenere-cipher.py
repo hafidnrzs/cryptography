1e916a62c056c71e41987f30e4ac36c8014b1469
hafidnrzs <hafidnurazis@gmail.com>
2024-10-06 00:46:14 +0700

    feat: add vigenere cipher algorithm

---
+++ vigenere-cipher/vigenere-cipher.py
@@ -0,0 +1,68 @@
+# Vigenere Cipher
+plain_text = "Hello World!"
+key = "polke"
+plain_text_length = len(plain_text)
+
+# ulang key agar panjang key = panjang plain text
+generated_key = key
+while (plain_text_length > len(generated_key)):
+    generated_key += key
+# trim key yang melebihi panjang
+generated_key = generated_key[:len(plain_text)]
+
+# enkripsi
+cipher_text = ""
+ascii_plain_text = []
+ascii_key = []
+for index in range(plain_text_length):
+    if plain_text[index].isalpha():
+        # mengubah ASCII menjadi indeks 0-25
+        ascii_plain_text.append(
+            ord(plain_text[index]) - 65
+            if plain_text[index].isupper()
+            else ord(plain_text[index]) - 97
+        )
+        ascii_key.append(
+            ord(generated_key[index]) - 65
+            if generated_key[index].isupper()
+            else ord(generated_key[index]) - 97
+        )
+        # mencari ciphertext
+        value = (ascii_plain_text[index] + ascii_key[index]) % 26
+        cipher_text += chr(value +
+                           65) if plain_text[index].isupper() else chr(value + 97)
+    else:
+        ascii_plain_text.append(ord(plain_text[index]))
+        ascii_key.append(ord(generated_key[index]))
+        cipher_text += plain_text[index]
+
+# dekripsi
+decrypted_text = ""
+cipher_text_length = len(cipher_text)
+ascii_cipher_text = []
+ascii_key = []
+for index in range(cipher_text_length):
+    if cipher_text[index].isalpha():
+        # mengubah ASCII menjadi indeks 0-25
+        ascii_cipher_text.append(
+            ord(cipher_text[index]) - 65
+            if cipher_text[index].isupper()
+            else ord(cipher_text[index]) - 97
+        )
+        ascii_key.append(
+            ord(generated_key[index]) - 65
+            if generated_key[index].isupper()
+            else ord(generated_key[index]) - 97
+        )
+        # mencari ciphertext
+        value = (ascii_cipher_text[index] - ascii_key[index]) % 26
+        decrypted_text += chr(value +
+                              65) if plain_text[index].isupper() else chr(value + 97)
+    else:
+        ascii_cipher_text.append(ord(plain_text[index]))
+        ascii_key.append(ord(generated_key[index]))
+        decrypted_text += plain_text[index]
+
+print("Plain text:", plain_text)
+print("Cipher text:", cipher_text)
+print("Decrypted text:", decrypted_text)
